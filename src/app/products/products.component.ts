05dcea8b18fae40605088c60d515271e0b55528b
Ed Gazdag <ed.gazdag@briebug.com>
2023-04-28 13:27:15 -0400

    Updates to clean code up after Cole's suggestions

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -8,41 +8,7 @@ import { ProductService } from '../services/product.service';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
-  productList: Product[];
-
   displayedColumns: string[] = ['id', 'name', 'category', 'quantity', 'price', 'delete'];
-  productCategories: string[];
-
-  currentProduct: Product;
-
-  constructor(public productService: ProductService) {
-    this.productList = productService.getProductList();
-    this.productCategories = productService.getProductCategories();
-    this.currentProduct = productService.getCurrentProduct();
-  }
-
-  addProduct() {
-    this.productService.addProduct();
-    this.getCurrentProductData();
-  }
-
-  updateProduct(product: Product) {
-    this.productService.updateProduct(product);
-    this.getCurrentProductData();
-  }
-
-  updateProductDetails(product: Product) {
-    this.productService.updateProductDetails(product);
-    this.getCurrentProductData();
-  }
-
-  deleteProduct(id: number) {
-    this.productService.deleteProduct(id);
-    this.getCurrentProductData();
-  }
-
-  getCurrentProductData() {
-    this.productList = this.productService.getProductList();
-    this.currentProduct = this.productService.getCurrentProduct();
-  }
+  
+  constructor(public productService: ProductService) {}
 }
